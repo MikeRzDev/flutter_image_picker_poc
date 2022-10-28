67de64b877fa1d659f0ae948dbee7447fcfe8120
Mike Ruiz <miguel.ruiz>
2022-10-28 00:12:19 -0500

    finished example

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,10 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
@@ -38,10 +41,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<XFile>? _imageFileList;
+  List<String>? _imagePathList;
 
-  void _setImageFileListFromFile(XFile? value) {
-    _imageFileList = value == null ? null : <XFile>[value];
+  void _setImageFileListFromFile(String? path) {
+    print('image path: ${path}');
+    _imagePathList = path == null ? null : <String>[path];
   }
 
   dynamic _pickImageError;
@@ -50,15 +54,68 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final ImagePicker _picker = ImagePicker();
 
+  // do it when the action page is closed
+  Future<void> deleteIOSImageCache() async {
+    final appBaseFolder = await getApplicationDocumentsDirectory();
+    final appTempFolder = appBaseFolder.path.replaceAll("/Documents", "/tmp");
+    final jpegFileList = await Directory(appTempFolder).list().where((element) => element.path.endsWith(".jpg")).toList();
+    for (var file in jpegFileList) {
+      await file.delete();
+    }
+  }
+
+  // do it when the action page is closed
+  Future<void> deleteAndroidImageCache() async {
+    final appTempFolder = await getTemporaryDirectory();
+    final jpegFileList = await appTempFolder.list().where((element) => element.path.endsWith(".jpg")).toList();
+    for (var file in jpegFileList) {
+      await file.delete();
+    }
+  }
+
+
+  Future<void> createPhotoDirectoryIfNotExists() async {
+    final appBaseFolder = Platform.isAndroid ? await getExternalStorageDirectory() : await getApplicationDocumentsDirectory();
+    final appBaseFolderPath = appBaseFolder?.path;
+    if (appBaseFolderPath == null) return;
+    final photosDirectory = Directory('$appBaseFolderPath/photos');
+    if (!await photosDirectory.exists()) {
+      await photosDirectory.create();
+    }
+  }
+
+
+  Future<void> deletePhotoDirectory() async {
+    final appBaseFolder = Platform.isAndroid ? await getExternalStorageDirectory() : await getApplicationDocumentsDirectory();
+    final appBaseFolderPath = appBaseFolder?.path;
+    if (appBaseFolderPath == null) return;
+    final photosDir = Directory('$appBaseFolderPath/photos');
+    if (await photosDir.exists()) {
+      await photosDir.delete(recursive: true);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 0), () async {
+      final appDocDir = await getExternalStorageDirectory().then((dir) => dir?.path ?? '');
+      final appDocPath = '$appDocDir/photos';
+      final jpegFileList = await Directory(appDocPath).list().where((element) => element.path.endsWith(".jpg")).toList();
+      print('list files');
+      for (var file in jpegFileList) {
+        print(file.path);
+      }
+    });
+  }
+
   Future<void> _pickImageFromGallery(BuildContext context) async {
     if (Platform.isIOS) {
       var status = await Permission.photos.status;
       if (status.isDenied) {
         final result = await Permission.photos.request();
         if (result.isDenied) {
-          {
-            return;
-          }
+          return;
         }
       } else if (status.isPermanentlyDenied) {
         await showDialog(
@@ -82,9 +139,10 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     try {
-      final List<XFile> pickedFileList = await _picker.pickMultiImage();
+      final pickedXFileList = await _picker.pickMultiImage();
+      final pickedFilePathList = pickedXFileList.map((xfile) => xfile.path).toList();
       setState(() {
-        _imageFileList = pickedFileList;
+        _imagePathList = pickedFilePathList;
       });
     } catch (e) {
       setState(() {
@@ -122,14 +180,51 @@ class _MyHomePageState extends State<MyHomePage> {
             });
       }
     }
+    /*else if (Platform.isAndroid) {
+      var status = await Permission.storage.status;
+      print(status);
+      if (status.isDenied) {
+        final result = await Permission.storage.request();
+        if (result.isDenied) {
+          return;
+        }
+      } else if (status.isPermanentlyDenied) {
+        await showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Permission denied'),
+                content: const Text('Please enable storage to save picture in external storage'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      openAppSettings();
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              );
+            });
+      }
+    }*/
 
     try {
-      final XFile? pickedFile = await _picker.pickImage(
+      final pickedFile = await _picker.pickImage(
         source: ImageSource.camera,
       );
-      setState(() {
-        _setImageFileListFromFile(pickedFile);
-      });
+      final appBaseFolder = Platform.isAndroid ? await getExternalStorageDirectory() : await getApplicationDocumentsDirectory();
+      if (pickedFile?.path != null || appBaseFolder != null) {
+        await createPhotoDirectoryIfNotExists();
+        final timestamp = DateTime.now().millisecondsSinceEpoch;
+        final photoFilename = 'FR_$timestamp';
+        final photoFilePath = '${appBaseFolder!.path}/photos/$photoFilename.jpg';
+        final photoFile = await File(pickedFile!.path).copy(photoFilePath);
+        await ImageGallerySaver.saveFile(pickedFile.path);
+        setState(() {
+          _setImageFileListFromFile(photoFile.path);
+        });
+      }
     } catch (e) {
       setState(() {
         _pickImageError = e;
@@ -142,7 +237,7 @@ class _MyHomePageState extends State<MyHomePage> {
     if (retrieveError != null) {
       return retrieveError;
     }
-    if (_imageFileList != null) {
+    if (_imagePathList != null) {
       return Semantics(
         label: 'image_picker_example_picked_images',
         child: ListView.builder(
@@ -152,10 +247,10 @@ class _MyHomePageState extends State<MyHomePage> {
             // See https://pub.dev/packages/image_picker#getting-ready-for-the-web-platform
             return Semantics(
               label: 'image_picker_example_picked_image',
-              child: kIsWeb ? Image.network(_imageFileList![index].path) : Image.file(File(_imageFileList![index].path)),
+              child: kIsWeb ? Image.network(_imagePathList![index]) : Image.file(File(_imagePathList![index])),
             );
           },
-          itemCount: _imageFileList!.length,
+          itemCount: _imagePathList!.length,
         ),
       );
     } else if (_pickImageError != null) {
@@ -183,9 +278,9 @@ class _MyHomePageState extends State<MyHomePage> {
     if (response.file != null) {
       setState(() {
         if (response.files == null) {
-          _setImageFileListFromFile(response.file);
+          _setImageFileListFromFile(response.file?.path);
         } else {
-          _imageFileList = response.files;
+          _imagePathList = response.files?.map((xfile) => xfile.path).toList();
         }
       });
     } else {
@@ -233,6 +328,36 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              onPressed: () async {
+                if (Platform.isAndroid) {
+                  await deleteAndroidImageCache();
+                } else if (Platform.isIOS) {
+                  await deleteIOSImageCache();
+                }
+                if (!mounted) return;
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Image cache deleted')));
+              },
+              heroTag: 'image4',
+              tooltip: 'Delete image from cache',
+              child: const Icon(Icons.delete_outline),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              onPressed: () async {
+                await deletePhotoDirectory();
+                if (!mounted) return;
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Image folder deleted')));
+              },
+              heroTag: 'image3',
+              tooltip: 'Delete image from app folder',
+              child: const Icon(Icons.delete_forever),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(top: 16.0),
             child: FloatingActionButton(
